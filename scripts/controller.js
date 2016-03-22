1351a450fe0fdea3e73197fe6aafa21b33fd9024
Loquacious <Loquacious@users.noreply.github.com>
2016-03-22 15:52:36 -0400

    Moon by default for non-JS users

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,6 +1,7 @@
 window.onload = function () {
-	var header = document.getElementsByTagName('i')[0];
+	var header = document.getElementsByClassName('icon')[0];
 	header.style.cursor = 'pointer';
+	header.className = 'icon-sun';
 	header.onclick = function () {
 		Goodnight.toggle();
 		return false;
