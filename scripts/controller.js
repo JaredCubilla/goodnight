bdf3a3c4429d1a5a42f712e78d5c17435a6068b3
Loquacious <Loquacious@users.noreply.github.com>
2016-03-22 16:08:49 -0400

    Cleaned up scripts

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-	var header = document.getElementsByClassName('icon')[0];
+	var header = document.getElementsByClassName('icon-moon')[0];
 	header.style.cursor = 'pointer';
 	header.className = 'icon-sun';
 	header.onclick = function () {
